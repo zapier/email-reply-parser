830c3541abd02f3b70c7d5b4ec4bb83fe7cb878c
Royce Haynes <royce.haynes@gmail.com>
2012-11-08 15:51:14 -0700

    Added tests to verify fragments

--- tests/tests.py
+++ tests/tests.py
@@ -15,10 +15,25 @@ class EmailMessageTest(unittest.TestCase):
         message = self.get_email('email_1_1')
 
         self.assertEquals(2, len(message.fragments))
-        self.assertEquals([False, True], map(lambda x: x, [f.signature for f in message.fragments]))
-        self.assertEquals([False, True], map(lambda x: x, [f.hidden for f in message.fragments]))
+        self.assertEquals([False, True],
+            map(lambda x: x, [f.signature for f in message.fragments]))
+        self.assertEquals([False, True],
+            map(lambda x: x, [f.hidden for f in message.fragments]))
         self.assertTrue("folks" in message.fragments[0].content)
         self.assertTrue("riak-users" in message.fragments[1].content)
+
+    def test_hidden_body(self):
+        message = self.get_email('email_1_2')
+
+        self.assertEquals(6, len(message.fragments))
+        self.assertEquals([False, True, False, True, False, False],
+            map(lambda x: x, [f.quoted for f in message.fragments]))
+
+        self.assertEquals([False, False, False, False, False, True],
+            map(lambda x: x, [f.signature for f in message.fragments]))
+
+        self.assertEquals([False, False, False, True, True, True],
+            map(lambda x: x, [f.hidden for f in message.fragments]))
 
     @unittest.skip("")
     def test_multiline_reply_headers(self):
