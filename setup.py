23b05e0c7446595632421bfbe7c8d052eb46af6b
Royce Haynes <royce.haynes@gmail.com>
2012-11-11 13:45:46 -0700

    Fix pypi install issue in setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,14 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='email_reply_parser',
-    version='0.1.1',
+    version='0.1.2',
     description='A email parser library, making it easy to extract a signature, reply, or quote block from a text-formatted email.',
-    long_description=readme,
     author='Royce Haynes',
     author_email='royce.haynes@gmail.com',
     url='https://github.com/zapier/email-reply-parser',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests')),
     test_suite='tests.test_email_message'
 )
