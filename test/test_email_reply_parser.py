0f6ed25d1f0dfb573de6d41a0ada960b0b36f96e
Matt Robenolt <matt@ydekproductions.com>
2014-05-10 11:30:19 -0700

    assertEqual instead of assertEquals

--- test/test_email_reply_parser.py
+++ test/test_email_reply_parser.py
@@ -11,10 +11,10 @@ class EmailMessageTest(unittest.TestCase):
     def test_simple_body(self):
         message = self.get_email('email_1_1')
 
-        self.assertEquals(3, len(message.fragments))
-        self.assertEquals([False, True, True],
+        self.assertEqual(3, len(message.fragments))
+        self.assertEqual([False, True, True],
             map(lambda x: x, [f.signature for f in message.fragments]))
-        self.assertEquals([False, True, True],
+        self.assertEqual([False, True, True],
             map(lambda x: x, [f.hidden for f in message.fragments]))
         self.assertTrue("folks" in message.fragments[0].content)
         self.assertTrue("riak-users" in message.fragments[2].content)
@@ -22,14 +22,14 @@ class EmailMessageTest(unittest.TestCase):
     def test_reads_bottom_message(self):
         message = self.get_email('email_1_2')
 
-        self.assertEquals(6, len(message.fragments))
-        self.assertEquals([False, True, False, True, False, False],
+        self.assertEqual(6, len(message.fragments))
+        self.assertEqual([False, True, False, True, False, False],
             map(lambda x: x, [f.quoted for f in message.fragments]))
 
-        self.assertEquals([False, False, False, False, False, True],
+        self.assertEqual([False, False, False, False, False, True],
             map(lambda x: x, [f.signature for f in message.fragments]))
 
-        self.assertEquals([False, False, False, True, True, True],
+        self.assertEqual([False, False, False, True, True, True],
             map(lambda x: x, [f.hidden for f in message.fragments]))
 
         self.assertTrue("Hi," in message.fragments[0].content)
@@ -40,7 +40,7 @@ class EmailMessageTest(unittest.TestCase):
     def test_reads_top_post(self):
         message = self.get_email('email_1_3')
 
-        self.assertEquals(5, len(message.fragments))
+        self.assertEqual(5, len(message.fragments))
 
     def test_multiline_reply_headers(self):
         message = self.get_email('email_1_6')
@@ -57,19 +57,19 @@ class EmailMessageTest(unittest.TestCase):
     def test_complex_body_with_one_fragment(self):
         message = self.get_email('email_1_5')
 
-        self.assertEquals(1, len(message.fragments))
+        self.assertEqual(1, len(message.fragments))
 
     def test_verify_reads_signature_correct(self):
         message = self.get_email('correct_sig')
-        self.assertEquals(2, len(message.fragments))
+        self.assertEqual(2, len(message.fragments))
 
-        self.assertEquals([False, False],
+        self.assertEqual([False, False],
             map(lambda x: x, [f.quoted for f in message.fragments]))
 
-        self.assertEquals([False, True],
+        self.assertEqual([False, True],
             map(lambda x: x, [f.signature for f in message.fragments]))
 
-        self.assertEquals([False, True],
+        self.assertEqual([False, True],
             map(lambda x: x, [f.hidden for f in message.fragments]))
 
         self.assertTrue('--' in message.fragments[1].content)
