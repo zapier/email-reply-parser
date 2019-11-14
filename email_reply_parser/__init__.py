9b8bf601dba81b492c64ffa6a946b196ce364d19
atc0m <atcom21@gmail.com>
2019-11-14 19:51:46 +0000

    signature appended to hidden fragment

--- email_reply_parser/__init__.py
+++ email_reply_parser/__init__.py
@@ -85,7 +85,7 @@ class EmailMessage(object):
         self._MULTI_QUOTE_HDR_REGEX = r'(?!Le.*Le\s.+?a écrit.*>:)(Le\s(.+?)a écrit.*>:)'
 
     def en_support(self):
-        self.SIG_REGEX = re.compile(r'(--|__|-\w)|(^Sent from my (\w+\s*){1,3})')
+        self.SIG_REGEX = re.compile(r'(--|__|-\w)|(^Sent from (\w+\s*){1,6})')
         self.QUOTE_HDR_REGEX = re.compile('\s*On.*wrote:$')
         self.QUOTED_REGEX = re.compile(r'(>+)|((&gt;)+)')
         self._MULTI_QUOTE_HDR_REGEX = r'(?!On.*On\s.+?wrote:)(On\s(.+?)wrote:)'
@@ -154,15 +154,12 @@ class EmailMessage(object):
         is_quote_header = self.QUOTE_HDR_REGEX.match(line) is not None
         is_quoted = self.QUOTED_REGEX.match(line) is not None
         is_header = is_quote_header or self.HEADER_REGEX.match(line) is not None
-        if self.fragment:
-            if self.SIG_REGEX.match(line.strip()):
+        if self.fragment and self.SIG_REGEX.match(line.strip()):
                 self.fragment.signature = True
-                self._finish_fragment()
-
-        if self.fragment \
+                self.fragment.lines.append(line)
+        elif self.fragment \
                 and ((self.fragment.headers == is_header and self.fragment.quoted == is_quoted) or
                          (self.fragment.quoted and (is_quote_header or len(line.strip()) == 0))):
-
             self.fragment.lines.append(line)
         else:
             self._finish_fragment()
